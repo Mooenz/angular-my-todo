b7a2dddd91464ad671450591434574cff8b0701e
mooenz <joss92821@hotmail.com>
2021-12-03 13:27:00 -0500

    fix bug

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'angular-my-todo';
+  arrToDos: string[] = [
+    'Estudiar 2 horas enplatzi',
+    'Tomar un descanso de 30 minutos',
+    'Poner en practica lo aprendido',
+  ];
+
+  toDo: string = '';
+  classLi: string = '';
+  finish: boolean = false;
+
+  addToDos(argumentToDo: string) {
+    this.arrToDos.push(argumentToDo);
+    this.toDo = '';
+
+    console.log(
+      'Si lograste cambiar el bg de html o body ayudame =(, mi twitter es : MooenzDev'
+    );
+  }
+
+  deleteToDo(indice:number) {
+    this.arrToDos.splice(indice, 1);
+  }
+
+  finishToDo(indice: number) {
+    const indiceStr = indice.toString();
+    const element = document.querySelector(`#id${indiceStr}`) as HTMLElement;
+
+    element.classList.toggle('terminado');
+  }
 }
